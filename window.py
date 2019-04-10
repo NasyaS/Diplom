1447d997beae89f8286757695f3d46492f67755d
Nasya <sadykovaanastasia@yandex.ru>
2019-04-10 12:48:34 +0300

    format

--- window.py
+++ window.py
@@ -7,7 +7,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QSize, Qt, pyqtSlot
 from PyQt5.QtGui import QBrush, QPen, QPixmap
 from PyQt5.QtWidgets import (QFileDialog, QGraphicsPixmapItem, QGraphicsScene,
-							 QLabel, QMainWindow, QPushButton)
+                             QLabel, QMainWindow, QPushButton)
 from PyQt5.uic import loadUi
 
 import cv2
@@ -16,136 +16,139 @@ from qosm.common import QOSM
 
 
 class Window(QMainWindow):
-	def __init__(self):
-		QMainWindow.__init__(self)
-		self.count = 0
-		self.canvas_coords = []
-		self.drawingEnabled = False
-		self.states = {0: "Режим разметки", 1: "Режим просмотра"}
-		self.setMinimumSize(QSize(800, 500))
-		loadUi(r'ui.ui', self)
-		self.canvas_sz = (self.canvas.geometry().width(),
-						  self.canvas.geometry().height())
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.count = 0
+        self.canvas_coords = []
+        self.drawingEnabled = False
+        self.states = {0: "Режим разметки", 1: "Режим просмотра"}
+        self.setMinimumSize(QSize(800, 500))
+        loadUi(r'ui.ui', self)
+        self.canvas_sz = (self.canvas.geometry().width(),
+                          self.canvas.geometry().height())
 
-		self.filePath = ''
-		self.matrixList = {
+        self.filePath = ''
+        self.matrixList = {
 
-			'23.1x15.4': 23.1,
-			'6.16x4.14': 6.16,
-			# matrix size : max size of matrix size
-			'Redmi (3.93x2.95)': 3.93
-		}
-		self.building_height = 0  # visota zdania
-		self.distance = 0
-		self.focalLength = 0
-		self.matrix.addItems(self.matrixList.keys())
-		self.view.mapClicked.connect(self.onMapLClick)
+            '23.1x15.4': 23.1,
+            '6.16x4.14': 6.16,
+            # matrix size : max size of matrix size
+            'Redmi (3.93x2.95)': 3.93
+        }
+        self.building_height = 0  # visota zdania
+        self.distance = 0
+        self.focalLength = 0
+        self.matrix.addItems(self.matrixList.keys())
+        self.view.mapClicked.connect(self.onMapLClick)
 
-	def load_image(self, file_name):
-		self.canvas.resize(*self.canvas_sz)
-		file = QPixmap(file_name)
-		self.origin_sz = (file.size().width(), file.size().height())
-		self.pixmap = file.scaledToHeight(self.canvas.geometry().height())
-		if self.pixmap.size().width() < self.canvas.geometry().width():
-			self.canvas.resize(self.pixmap.size())
-		self.scene = QGraphicsScene(
-			0, 0, self.pixmap.size().width(), self.pixmap.size().height())
-		self.scene.addItem(QGraphicsPixmapItem(self.pixmap))
-		self.canvas.setScene(self.scene)
-		self.canvas.setRenderHint(QtGui.QPainter.HighQualityAntialiasing, True)
-		self.canvas.show()
-		self.canvas.mousePressEvent = self.drawmode
+    def load_image(self, file_name):
+        self.canvas.resize(*self.canvas_sz)
+        file = QPixmap(file_name)
+        self.origin_sz = (file.size().width(), file.size().height())
+        self.pixmap = file.scaledToHeight(self.canvas.geometry().height())
+        if self.pixmap.size().width() < self.canvas.geometry().width():
+            self.canvas.resize(self.pixmap.size())
+        self.scene = QGraphicsScene(
+            0, 0, self.pixmap.size().width(), self.pixmap.size().height())
+        self.scene.addItem(QGraphicsPixmapItem(self.pixmap))
+        self.canvas.setScene(self.scene)
+        self.canvas.setRenderHint(QtGui.QPainter.HighQualityAntialiasing, True)
+        self.canvas.show()
+        self.canvas.mousePressEvent = self.drawmode
 
-	def drawmode(self, event):
-		if not self.drawingEnabled:
-			return
-		x, y = event.x()+self.canvas.horizontalScrollBar().sliderPosition(), event.y() + \
-			self.canvas.verticalScrollBar().sliderPosition()
-		self.canvas_coords.append((x, y))
-		self.scene.addEllipse(x-2.5, y-2.5, 5, 5,
-							  QPen(QtCore.Qt.red), QtCore.Qt.red)
-		if len(self.canvas_coords) < 2:
-			return
-		self.scene.addLine(
-			*self.canvas_coords[1], *self.canvas_coords[0], QPen(QtCore.Qt.red, 3, Qt.SolidLine, Qt.SquareCap, Qt.RoundJoin))
-		def scale(w, h): return np.array(
-			(w*self.origin_sz[0]/self.pixmap.size().width(), h*self.origin_sz[1]/self.pixmap.size().height()))
-		M1, M2 = [scale(*item) for item in self.canvas_coords]
-		self.Len_line = np.linalg.norm(M2-M1)
-		self.canvas_coords = []
-		self.drawingEnabled = False
-		self.mark.setText(self.states[self.drawingEnabled])
+    def drawmode(self, event):
+        if not self.drawingEnabled:
+            return
+        x, y = event.x()+self.canvas.horizontalScrollBar().sliderPosition(), event.y() + \
+            self.canvas.verticalScrollBar().sliderPosition()
+        self.canvas_coords.append((x, y))
+        self.scene.addEllipse(x-2.5, y-2.5, 5, 5,
+                              QPen(QtCore.Qt.red), QtCore.Qt.red)
+        if len(self.canvas_coords) < 2:
+            return
+        self.scene.addLine(
+            *self.canvas_coords[1], *self.canvas_coords[0], QPen(QtCore.Qt.red, 3, Qt.SolidLine, Qt.SquareCap, Qt.RoundJoin))
+        def scale(w, h): return np.array(
+            (w*self.origin_sz[0]/self.pixmap.size().width(), h*self.origin_sz[1]/self.pixmap.size().height()))
+        M1, M2 = [scale(*item) for item in self.canvas_coords]
+        self.Len_line = np.linalg.norm(M2-M1)
+        self.canvas_coords = []
+        self.drawingEnabled = False
+        self.mark.setText(self.states[self.drawingEnabled])
 
-	@pyqtSlot()
-	def on_mark_clicked(self):
-		self.drawingEnabled = False if self.drawingEnabled else True
-		self.mark.setText(self.states[self.drawingEnabled])
+    @pyqtSlot()
+    def on_mark_clicked(self):
+        self.drawingEnabled = False if self.drawingEnabled else True
+        self.mark.setText(self.states[self.drawingEnabled])
 
-	@pyqtSlot()
-	def on_clear_clicked(self):
-		self.scene.clear()
-		self.scene.addItem(QGraphicsPixmapItem(self.pixmap))
-		self.canvas_coords = []
+    @pyqtSlot()
+    def on_clear_clicked(self):
+        self.scene.clear()
+        self.scene.addItem(QGraphicsPixmapItem(self.pixmap))
+        self.canvas_coords = []
 
-	def onMarkerRClick(self, key, lat, lng):
-		if self.checkBox.isChecked():
-			self.view.removeMarker(key)
+    def onMarkerRClick(self, key, lat, lng):
+        if self.checkBox.isChecked():
+            self.view.removeMarker(key)
 
-	@pyqtSlot()
-	def on_btnpath_clicked(self):
-		temp = QFileDialog.getOpenFileName(
-			self, 'Open file', '', 'Images (*.png *.jpg *jpeg)')[0]
-		self.path.setText(temp)
-		self.filePath = temp
-		self.load_image(temp)
+    @pyqtSlot()
+    def on_btnpath_clicked(self):
+        temp = QFileDialog.getOpenFileName(
+            self, 'Open file', '', 'Images (*.png *.jpg *jpeg)')[0]
+        self.path.setText(temp)
+        self.filePath = temp
+        self.load_image(temp)
 
-	def onMapLClick(self, latitude, longitude):
-		print(latitude, longitude)
-		self.cord_x, self.cord_y = latitude, longitude
-		self.count+=1
-		self.view.addMarker("Mark "+str(self.count), latitude, longitude)
+    def onMapLClick(self, latitude, longitude):
+        print(latitude, longitude)
+        self.cord_x, self.cord_y = latitude, longitude
+        self.count += 1
+        self.view.addMarker("Mark "+str(self.count), latitude, longitude)
 
-	@pyqtSlot()
-	def on_go_clicked(self):
-		self.calc()
+    @pyqtSlot()
+    def on_go_clicked(self):
+        self.calc()
 
+    def panMap(self, lat, lng,):
+        frame = self.view.page()
+        frame.runJavaScript('mymap.panTo(L.latLng({}, {}));'.format(lat, lng))
 
-	def panMap(self, lat, lng,):
-		frame = self.view.page()
-		frame.runJavaScript('mymap.panTo(L.latLng({}, {}));'.format(lat, lng))
+    def calc(self):
+        image = Image.open(self.filePath)
+        print(self.filePath)
 
-	def calc(self):
-		image = Image.open(self.filePath)
-		print(self.filePath)
+        exif = {
+            ExifTags.TAGS[k]: v
+            for k, v in image._getexif().items()
+            if k in ExifTags.TAGS
+        }
+        print(exif)
 
-		exif = {
-			ExifTags.TAGS[k]: v
-			for k, v in image._getexif().items()
-			if k in ExifTags.TAGS
-		}
-		print(exif)
+        width, height = image.size
+        print(width)
+        print(height)
 
-		width, height = image.size
-		print(width)
-		print(height)
+        self.focalLength = exif['FocalLength'][0]/exif['FocalLength'][1]
 
-		self.focalLength = exif['FocalLength'][0]/exif['FocalLength'][1]
+        size_on_matrix = (self.matrixList[self.matrix.currentText(
+        )]*self.Len_line)/np.max(([height, width]))
 
-		size_on_matrix = (self.matrixList[self.matrix.currentText()]*self.Len_line)/np.max(([height, width]))
+        self.building_height = float(self.height.text())
 
-		self.building_height = float(self.height.text())
+        self.distance = self.focalLength * \
+            ((self.building_height/size_on_matrix)+1)
 
-		self.distance = self.focalLength * \
-			((self.building_height/size_on_matrix)+1)
+        print(self.distance, self.distance*0.1)
 
+        if self.ch1.isChecked():
+            m = 1
+        if self.ch2.isChecked():
+            m = 2
+        if self.ch3.isChecked():
+            m = 3
+        if self.ch4.isChecked():
+            m = 4
 
-		print(self.distance, self.distance*0.1)	
+        self.view.addCircle(self.distance, m)
 
-		if self.ch1.isChecked(): m = 1
-		if self.ch2.isChecked(): m = 2
-		if self.ch3.isChecked(): m = 3
-		if self.ch4.isChecked(): m = 4
-
-		self.view.addCircle(self.distance, m)
-
-		self.dist.setText(str(self.distance))
+        self.dist.setText(str(self.distance))
